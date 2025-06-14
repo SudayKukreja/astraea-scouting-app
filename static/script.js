40a7db61577a20c8558f535b92d7fc591ad0b085
Heisenberg1525 <sudaykukreja08@gmail.com>
2025-06-14 08:58:11 -0400

    updated  fetch URL

--- static/script.js
+++ static/script.js
@@ -69,7 +69,7 @@ document.getElementById('scout-form').addEventListener('submit', async (e) => {
   };
 
   try {
-    const res = await fetch('http://localhost:5000/submit', {
+    const res = await fetch('/submit', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
