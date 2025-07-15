5a8e0beb5d5ac9b68e2298f2df93038ae5537ce9
Heisenberg1525 <sudaykukreja08@gmail.com>
2025-07-15 09:10:00 -0400

    Auto deploy commit at 2025-07-15 09:09:59 Changes: M deploy.ps1  M templates/index.html

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,10 @@
-
 $datetime = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
+$gitStatus = git status --short | Out-String
+$gitSummary = if ($gitStatus) { $gitStatus.Trim() } else { "No changes detected" }
+
+$commitMessage = "Auto deploy commit at $datetime`nChanges:`n$gitSummary"
+
 git add .
-git commit -m "Auto deploy commit at $datetime"
+git commit -m $commitMessage
 git push origin main
